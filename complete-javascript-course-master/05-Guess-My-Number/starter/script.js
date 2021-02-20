0fa8bda6b53154075f4e5b09b8bffea60cfac29e
Yoto Dikov <yotodikov@gmail.com>
2021-02-21 00:24:53 +0200

    'Gues my number' Coding challenge... done.

--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,55 @@
 'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = '🎉 Correct Number';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+let number = Math.trunc(Math.random() * 20 + 1);
+console.log(number);
+
+let message = document.querySelector('.message');
+let scoreCounter = document.querySelector('.score');
+let highScore = document.querySelector('.highscore');
+let currentScore;
+
+document.querySelector('.check').addEventListener('click', function () {
+  let guessNum = document.querySelector('.guess').value;
+  if (guessNum < number) {
+    message.textContent = '📉 Too low!';
+    scoreCounter.textContent -= 1;
+  } else if (guessNum > number) {
+    message.textContent = '📈 Too high!';
+    scoreCounter.textContent -= 1;
+  } else {
+    message.textContent = '🎉 Correct Number';
+    document.querySelector('.number').textContent = guessNum;
+    document.querySelector('.number').style.width = '30rem';
+    document.body.style.backgroundColor = '#60b347';
+    currentScore = scoreCounter.textContent;
+    if (Number(currentScore) > Number(highScore.textContent)) {
+      highScore.textContent = currentScore;
+    }
+  }
+});
+
+// const btnAgain = document.querySelector('body > header > button');
+// console.log(btnAgain);
+
+document
+  .querySelector('body > header > button')
+  .addEventListener('click', function () {
+    message.textContent = 'Start guessing...';
+    scoreCounter.textContent = 20;
+    document.querySelector('.number').style.width = '15rem';
+    document.body.style.backgroundColor = '#222';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    number = Math.trunc(Math.random() * 20 + 1);
+    console.log(number);
+  });
