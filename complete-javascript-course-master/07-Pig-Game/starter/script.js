a399049b0de82a136876102f5dd4ea95858b7996
Yoto Dikov <yotodikov@gmail.com>
2021-02-24 22:32:06 +0200

    No point.

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -6,31 +6,7 @@ const rollBtn = document.querySelector('.btn--roll');
 const holdBtn = document.querySelector('.btn--hold');
 const newGameBtn = document.querySelector('.btn--new');
 
-newGameBtn.addEventListener('click', function () {
-  player1.totalScore.textContent = 0;
-  player2.totalScore.textContent = 0;
-  if (!player1.status.classList.contains('player--active')) {
-    player2.status.classList.remove('player--active');
-    player1.status.classList.add('player--active');
-  }
-});
-
-// const swichPlayersTurn = function (p1, p2) {
-//   player.currentScore.textContent = 0;
-//   player.myTurn = false;
-//   nextPlayer.myTurn = true;
-// };
-const playerManipulation = function (player, nextPlayer) {
-  const rollNumber = Math.trunc(Math.random() * 6 + 1);
-  dice.src = `dice-${rollNumber}.png`;
-  if (rollNumber > 1) {
-    let num = Number(player.currentScore.textContent);
-    num += rollNumber;
-    player.currentScore.textContent = num;
-  } else {
-    swich(player1, player2);
-  }
-};
+let dice = document.querySelector('.dice');
 
 // Players
 let player1 = {
@@ -53,18 +29,26 @@ player1.totalScore.textContent = 0;
 player2.totalScore.textContent = 0;
 
 // Display dice
-let dice = document.querySelector('.dice');
 
 // EVENT LISTENERS
-function roll() {
-  if (player1.myTurn) {
-    playerManipulation(player1, player2);
-  } else {
-    playerManipulation(player2, player1);
+newGameBtn.addEventListener('click', function () {
+  player1.totalScore.textContent = 0;
+  player2.totalScore.textContent = 0;
+  if (!player1.status.classList.contains('player--active')) {
+    player2.status.classList.remove('player--active');
+    player1.status.classList.add('player--active');
   }
-}
+});
 
+holdBtn.addEventListener('click', hold);
 rollBtn.addEventListener('click', roll);
+rollBtn.addEventListener('keydown', function (e) {
+  if (e.key === 'Enter' || e.key === 'Space') {
+    roll();
+  }
+});
+
+// FUNCTIONS
 
 function currnetPlayer(p1, p2) {
   return p1.myTurn ? p1 : p2;
@@ -83,7 +67,6 @@ function hold() {
 
   swich(player1, player2);
 }
-holdBtn.addEventListener('click', hold);
 
 function swich(player1, player2) {
   if (player1.myTurn === true) {
@@ -92,8 +75,6 @@ function swich(player1, player2) {
     player1.status.classList.remove('player--active');
     player2.myTurn = true;
     player2.status.classList.add('player--active');
-
-    console.log(true);
   } else {
     player2.currentScore.textContent = 0;
     player2.myTurn = false;
@@ -103,9 +84,28 @@ function swich(player1, player2) {
   }
 }
 
+function roll() {
+  if (player1.myTurn) {
+    playerManipulation(player1);
+  } else {
+    playerManipulation(player2);
+  }
+}
 function winner(winnerPlayer) {
   winnerPlayer.status.classList.remove('player--active');
   winnerPlayer.status.classList.add('player--winner');
   holdBtn.removeEventListener('click', hold);
   rollBtn.removeEventListener('click', hold);
 }
+
+const playerManipulation = function (player) {
+  const rollNumber = Math.trunc(Math.random() * 6 + 1);
+  dice.src = `dice-${rollNumber}.png`;
+  if (rollNumber > 1) {
+    let num = Number(player.currentScore.textContent);
+    num += rollNumber;
+    player.currentScore.textContent = num;
+  } else {
+    swich(player1, player2);
+  }
+};
